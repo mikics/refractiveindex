f58ef97ee7d97050e94d3e96ea23df80119af045
Michele Castriotta <mich.castriotta@gmail.com>
2022-11-21 17:37:14 +0100

    Change setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import sys
 
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 if sys.version_info < (3, 8):
     print("Python 3.8 or higher required, please upgrade.")
@@ -18,9 +18,9 @@ REQUIREMENTS = [
 setup(
     name='refractiveindex', version=VERSION,
     author='Michele Castriotta',
-    description='A Python3 + SQLite wrapper for the refractiveindex database',
+    description='A Python wrapper for the refractiveindex.info database',
     long_description='',
-    packages=["refractiveindex"],
+    packages=find_namespace_packages(where="./"),
     install_requires=REQUIREMENTS,
     include_package_data=True,
-    package_data={'refractiveindex': ['data.txt', 'refractiveindex.info-database/database/data/*/*/*.yml']},)
+    package_data={"refractiveindex.ridb.database.data.*.*": ["*.yml"]},)
